525e56713aaace91699e5da1576172034f452b89
Phuricha Wichitjomthong <wichitjomthong_p@slipakorn.edu>
2022-08-20 22:58:50 +0700

    add work1

--- lib/game1.dart
+++ lib/game1.dart
@@ -8,16 +8,18 @@ enum Result {
 
 class Game {
   static const defaultMaxRandom = 100;
-  int? _answer;
+  int? answer;
   int _guessCount = 0;
   static final List<int> guessCountList = [];
 
   Game({int maxRandom = defaultMaxRandom}) {
     var r = Random();
-    _answer = r.nextInt(maxRandom) + 1;
-    print('The answer is $_answer');
+    answer = r.nextInt(maxRandom) + 1;
+    print('The answer is $answer');
   }
-
+  int? reAns(){
+    return answer;
+}
   int get guessCount {
     return _guessCount;
   }
@@ -28,9 +30,9 @@ class Game {
 
   Result doGuess(int num) {
     _guessCount++;
-    if (num > _answer!) {
+    if (num > answer!) {
       return Result.tooHigh;
-    } else if (num < _answer!) {
+    } else if (num < answer!) {
       return Result.tooLow;
     } else {
       return Result.correct;
--- lib/page/game_page.dart
+++ lib/page/game_page.dart
@@ -8,12 +8,15 @@ class GamePage extends StatefulWidget {
 
 //--------------------------------------------------//
 class _GamePageState extends State<GamePage> {
+  //เก็บตัวอักษรไว้จากผู้พิม มาจากหน้าtext fild
+  //TextEditingController คือช่องว่าง
   final _controller = TextEditingController();
   final _game = Game();
 
   // state variable
-  var _feedbackText = '';
-  var _showTestButton = false;
+  var _ans = '';
+  var _feedbackText = ''; //เก็บข้อความไปแสดงผลที่จอเเว็ป
+  var _showTestButton = false; //เก็บค่าแสดงไอคอนปุ่มtest
 
 //------------------------
   void handleClickGuess() {
@@ -33,41 +36,53 @@ class _GamePageState extends State<GamePage> {
       });
     } else {
       var result = _game.doGuess(guess);
+      var A = _game.reAns();
+      setState(() {
+        _ans = 'The answer is $A';
+      });
       if (result == Result.tooHigh) {
         // ทายมากไป
         print('Too high');
         setState(() {
-          _feedbackText = 'Too high, please try again';
+          _feedbackText = '$guess Too high, please try again';
         });
       } else if (result == Result.tooLow) {
         // ทายน้อยไป
         print('Too low');
         setState(() {
-          _feedbackText = 'Too low, please try again';
+          _feedbackText = '$guess Too low, please try again';
         });
       } else {
         // ทายถูก
         print('Correct');
         setState(() {
-          _feedbackText = 'Good job!';
+          _feedbackText = '$guess Good job!';
         });
       }
     }
   }
 
-//---------------------------------
+//---------------ตัวที่แสดงหน้าเว็ป------------------
   @override
   Widget build(BuildContext context) {
+    //ตัวจัดรูปแบบทั่วไป
     return Scaffold(
+      //แทบข้อเรื่องเว็ป
       appBar: AppBar(
         title: const Text('Guess the Number'),
       ),
+      //การแสดงผลทั้งหมดอยู่ภายใตคำสั่ง Center เซตค่ากึ่งกลาง
       body: Center(
+        //childเก็บwidgetได้ตัวเดียว ตั้งค่าว่าจะจัดเป็นแนวไหน col>ตั้ง row>นอน
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          //จัดระเบียบระหว่างบรรทัด
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          //children เก็บ widget ได้ทีละหลายๆตัวได้
           children: [
             const Text('Please enter number between 1 and 100'),
+            //ช่องที่ให้ผู้ใช้พิม
             TextField(
+              //controller: TextEditingController(),
               controller: _controller,
               decoration: const InputDecoration(
                 border: OutlineInputBorder(),
@@ -75,19 +90,23 @@ class _GamePageState extends State<GamePage> {
               ),
             ),
             // Callback function
+            //ปุ่มกด
             ElevatedButton(
+              //ชื่อเมธอดที่จะส่งค่าไป
               onPressed: handleClickGuess,
               child: const Text('GUESS'),
             ),
+
+            Text(_ans),
             Text(_feedbackText),
 
-
+            //ปุ่มกด
             ElevatedButton(
               style: OutlinedButton.styleFrom(
-                backgroundColor: _showTestButton ? Colors.green : Colors.red,
+                backgroundColor: _showTestButton ? Colors.black : Colors.pink,
               ),
               onPressed: (){},
-              child: Text('TEST'),
+              child: const Text('TEST'),
             ),
             Icon(_showTestButton ? Icons.print :Icons.access_time),
           ],
